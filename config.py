111f44174b900751bfdad904015ba04182c66ef2
Koina111 <lifemeditation19@gmail.com>
2024-07-28 15:09:02 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY VENOMxCRAZY
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7187634138:AAFmD1aGD4MbIFLK6f1COKWSxl-nC4qhtP8")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("koina1997")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("715186742")
+
+
+
+
+
+
+
+  
